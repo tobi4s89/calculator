86547011c1e401fa306b2ba604f52f9ee0cb8d8d
tobiasvanegten <tvanegten@hotmail.com>
2019-11-13 12:12:34 +0100

    Added data option json object

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ new Vue({
   },
   beforeMount () {
     const inventory = this.$el.attributes['data-inventory']
-    if (inventory !== undefined) this.inventory = JSON.parse(inventory.value)
+    if (inventory !== undefined) this.inventory = JSON.parse(inventory.value).data
   },
   render (createElement) {
     const self = this
